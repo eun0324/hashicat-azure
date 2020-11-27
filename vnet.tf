faf0a47ed18ec6fdfb864314f9e74b8ce1b1e529
ejmo <ejmo@cloocus.com>
2020-11-27 07:24:25 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,6 @@
 module "network" {
   source  = "app.terraform.io/eunjung-traning/network/azurerm"
   version = "3.2.1"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   # insert required variables here
 }
